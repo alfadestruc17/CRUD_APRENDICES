e73f4dda63d254eddfc5ef324ecca1bc4dcac14e
Samuel <rene28609@gmail.com>
2025-04-11 18:24:10 -0500

    segunda fase arreglo de la vista principal

--- index.php
+++ index.php
@@ -3,8 +3,7 @@ require_once("C://laragon/www/CRUD_APRENDICES/views/head/header.php");
 ?>
 
 
-
-
+<a href="/CRUD_APRENDICES/views/aprendiz/create.php" class="btn btn-primary"> Agregar un Aprendiz</a>
 
 
 
--- views/head/header.php
+++ views/head/header.php
@@ -9,25 +9,28 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="c://laragon/www/CRUD_APRENDICES/index.php">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Aprendices
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="c://laragon/www/CRUD?APRENDICES/views/index.php">Ver aprendices</a></li>
-              <li><a class="dropdown-item" href="#">Crear aprendices</a></li>
-            </ul>
-          </li>
-        </ul>
-        
+  <div class="container-fluid bg-dark p-2 mb-3">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="c://laragon/www/CRUD_APRENDICES/index.php">inicio</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Aprendices
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="/CRUD_APRENDICES/views/aprendiz/index.php">Ver aprendices</a></li>
+                <li><a class="dropdown-item" href="/CRUD_APRENDICES/views/aprendiz/create.php">Crear aprendices</a></li>
+              </ul>
+            </li>
+          </ul>
+
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
+  <div class="container-fluid">
